c89d66e4e04174b0659782bd49d88c26d8cddda2
mahendra-deshmukh <mahendra.deshmukh@hotmail.com>
2023-11-04 17:26:33 +0530

    Assignment 03 Updated

--- Assignment_03/Que1/src/com/sunbeam/AllConversions.java
+++ Assignment_03/Que1/src/com/sunbeam/AllConversions.java
@@ -85,28 +85,40 @@ public class AllConversions {
 		return res;
 	}
 
-	public void infixToPostfix(String str) {
-		String split[] = str.split(str);
+	public static String infixToPostfix(String str) {
+		String split[] = str.split(" ");
 		StringBuilder postfix = new StringBuilder();
 		Stack<String> stack = new Stack<>();
 		for (int i = 0; i < split.length; i++) {
-			if (Character.isDigit(split[i].charAt(i)))
-				postfix.append(split[i]);
-			else {
-				if (split[i] == "(") {
-					stack.push(split[i]);
-				} else if (stack.isEmpty())
-					stack.push(split[i]);
-				else {
-					if (operatorPriority(stack.peek()) >= operatorPriority(split[i]))
-						postfix.append(split[i]);
-					else
-						stack.push(split[i]);
+			if (Character.isDigit(split[i].charAt(0))) {
+			postfix.append(split[i]+" ");
+			}
+			else if (split[i].equals("(")) {				
+				stack.push(split[i]);
+			} 
+			else if (split[i].equals(")")) {
+				while (!stack.peek().equals("(")) {
+					postfix.append(stack.pop()+" ");
 				}
+				stack.pop();
+			} else {
+				while (!stack.isEmpty() && operatorPriority(stack.peek()) >= operatorPriority(split[i]))
+					postfix.append(stack.pop()+" ");
+				stack.push(split[i]);
 			}
 		}
-	}
+		while (!stack.isEmpty()) {
+			postfix.append(stack.pop()+" ");
+		}
 
+		return postfix.toString().trim();
+	}
+	
+	public static void infixToPrefix(String str)
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		String postFix = "8 3 * 10 - 12 + 2 / 5 %";
 		int resultPostFix = postfixEvaluation(postFix);
@@ -115,6 +127,10 @@ public class AllConversions {
 		String preFix = "% / + - * 8 3 10 12 2 5";
 		int resultPrefix = prefixEvaluation(preFix);
 		System.out.println("Result of prefix : " + resultPrefix);
+
+		String inFix = "1 $ 9 + 3 * 4 - ( 6 + 18 / 2 ) + 7";
+		String toPostFix = infixToPostfix(inFix);
+		System.out.println("Infix to Postfix : "+toPostFix);
 	}
 
 }
